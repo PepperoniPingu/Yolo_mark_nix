9e031f2a68b4d75fcbd1de8a2fc1d4b9d727c129
Hugo Frisk <hugopaf@outlook.com>
2025-03-14 15:45:26 +0100

    remove pause from start script

--- nix_mark.sh
+++ nix_mark.sh
@@ -2,6 +2,3 @@ echo     Example how to start marking bouded boxes for training set Yolo v2
 
 
 result/bin/yolo_mark x64/Release/data/img x64/Release/data/train.txt x64/Release/data/obj.names
-
-
-pause
